062c8ecab127521fff4a0e02b3bd60927fb6e37a
Vitaliy <Gallifrey46@gmail.com>
2018-04-19 19:26:22 +0300

    Изменение артефакта, изменение имени класса Application

--- src/main/java/com/doomsday/caloriecalculator/ServerApplication.java
+++ src/main/java/com/doomsday/caloriecalculator/ServerApplication.java
@@ -1,12 +1,12 @@
-package com.doomsday.demo;
+package com.doomsday.caloriecalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 }
--- src/test/java/com/doomsday/caloriecalculator/DemoApplicationTests.java
+++ src/test/java/com/doomsday/caloriecalculator/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.doomsday.demo;
+package com.doomsday.caloriecalculator;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
